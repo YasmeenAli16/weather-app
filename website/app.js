e7fe766caf9dee0ff99a5b016b930d2b43763572
YasmeenAli16 <yasmeenali1698@gmail.com>
2021-05-06 22:52:08 +0200

    Update data

--- website/app.js
+++ website/app.js
@@ -2,7 +2,7 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth()+1) +'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // create base url using zip code and personal API key for OpenWeatherMap
 const apiKey = "&appid=107cc37f2d8b88b0a640991055d1dca5&units=metric";
