1186107dc9c104108542815dd7a4fcc748cbd80a
YasmeenAli16 <yasmeenali1698@gmail.com>
2021-05-06 20:51:13 +0200

    Add global variables

--- website/app.js
+++ website/app.js
@@ -4,6 +4,8 @@
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
-
+// create base url using zip code and personal API key for OpenWeatherMap
+const apiKey = "&appid=107cc37f2d8b88b0a640991055d1dca5&units=metric";
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
 
 
